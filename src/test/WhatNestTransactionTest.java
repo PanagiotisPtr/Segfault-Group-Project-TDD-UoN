a93d4a146a0511881d6eb7c9c929db8b3cab0b65
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2019-03-25 20:07:16 +0000

    removed unused import

--- src/test/WhatNestTransactionTest.java
+++ src/test/WhatNestTransactionTest.java
@@ -3,7 +3,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Calendar;
 
