0fad15b79db1567fc57332448fe09e36858ebbb7
Berk Demir <psybd2@nottingham.ac.uk>
2019-03-28 14:06:59 +0000

    isComplete

---
+++ src/test/WhatNestTransactionTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WhatNestTransactionTest {
+    @Nested
+    class IsComplete{
+
+        /*
+         * Test complete name and complete value
+         * Written by: Berk Demir 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void allComplete(){
+            WhatNestTransaction t = new WhatNestTransaction();
+            t.setTransactionName("Good");
+            t.setTransactionValue(new BigDecimal(10));
+            assertTrue(t.isComplete());
+        }
+
+        /*
+         * Test complete name and incomplete value
+         * Written by: Berk Demir 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void nameComplete(){
+            WhatNestTransaction t = new WhatNestTransaction();
+            t.setTransactionName("Good");
+            assertFalse(t.isComplete());
+        }
+        /*
+         * Test incomplete name and complete value
+         * Written by: Berk Demir 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void valueComplete(){
+            WhatNestTransaction t = new WhatNestTransaction();
+            t.setTransactionValue(new BigDecimal(10));
+            assertFalse(t.isComplete());
+        }
+        /*
+         * Test incomplete name and incomplete value
+         * Written by: Berk Demir 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void nothingComplete() {
+            WhatNestTransaction t = new WhatNestTransaction();
+            assertFalse(t.isComplete());
+        }
+    }
+}
