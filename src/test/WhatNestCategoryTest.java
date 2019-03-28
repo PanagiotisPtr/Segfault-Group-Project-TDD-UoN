c789012c13ec91889fd7af60157ad5dc5d4c1984
Berk Demir <psybd2@nottingham.ac.uk>
2019-03-28 15:59:38 +0000

    resetBudgetSpend

---
+++ src/test/WhatNestCategoryTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+class WhatNestCategoryTest {
+    @Nested
+    class ResetBudgetSpend{
+
+        /*
+         * Test that it sets Spend in a category to zero
+         * Written by: Berk Demir 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void checkResetBudget(){
+            WhatNestCategory cat = new WhatNestCategory();
+            BigDecimal zero = new BigDecimal("0");
+            cat.resetBudgetSpend();
+            assertEquals(0, cat.CategoryBudget().compareTo(zero));
+        }
+    }
+}
