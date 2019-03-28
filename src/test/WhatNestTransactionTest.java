42306bcf4d7599b9af5fcdc9947fa6b198a5f9b9
taradilley <pmytrd@nottingham.ac.uk>
2019-03-28 16:38:42 +0000

    Completed test1.11

---
+++ src/test/WhatNestTransactionTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WhatNestTransactionTest {
+
+    @Nested
+    class toString {
+
+        /*
+         * Checks for a clear representation (valid transaction date, name and value)
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+
+        @Test
+        void clearRepresentation() {
+            Date before = new Date();
+            BigDecimal num = new BigDecimal("39.99");
+            WhatNestTransaction transaction = new WhatNestTransaction("Spending", num, 4);
+            assertEquals("Spending - £" + num + before, transaction.toString());
+        }
+
+        /*
+         * Checks transactionValue is not missing
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void checkParameters1() {
+            assertThrows(Exception.class, () -> {new WhatNestTransaction("Spending", null , 4);});
+        }
+
+        /*
+         * Checks transactionName is not missing
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void checkParameters2() {
+            BigDecimal num = new BigDecimal("39.99");
+            assertThrows(Exception.class, () -> {new WhatNestTransaction("", num , 4);});
+        }
+
+        /*
+         * Checks transactionName and transactionValue are not missing
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void checkParameters3() {
+            assertThrows(Exception.class, () -> {new WhatNestTransaction("", null , 4);});
+        }
+    }
+}
