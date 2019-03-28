0b4f3a3872e969c571ad3b56e54fb8a7aa52ec6b
taradilley <pmytrd@nottingham.ac.uk>
2019-03-28 22:42:29 +0000

    Finished test2.9

---
+++ src/test/WhatNestCategoryTest.java
@@ -0,0 +1,103 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WhatNestCategoryTest {
+    @Nested
+
+    class removeExpense{
+
+        /*
+         * Checks that BigDecimal type is accepted
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void acceptsBigDecimal() {
+
+            WhatNestCategory cat = new WhatNestCategory("Example");
+            BigDecimal num1 = new BigDecimal("100.00");
+            BigDecimal num2 = new BigDecimal("1.3458934895");
+
+            cat.addExpense(num1);
+            cat.removeExpense(num2);
+
+            BigDecimal result = num1.subtract(num2);
+
+            assertEquals(result, cat.CategorySpend());
+        }
+
+        /*
+         * Checks 0 is not subtracted
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void noZero() {
+            WhatNestCategory cat = new WhatNestCategory("Example");
+            assertThrows(Exception.class, () -> {
+                cat.removeExpense(new BigDecimal("0"));
+            });
+        }
+
+        /*
+         * Checks that negative numbers are not accepted
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void noNegative() {
+
+            WhatNestCategory cat = new WhatNestCategory("Example");
+            assertThrows(Exception.class, () -> {
+                cat.removeExpense(new BigDecimal("-10000.12"));
+            });
+        }
+
+        /*
+         * Checks that large positive numbers are accepted
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void largePositive() {
+
+            WhatNestCategory cat = new WhatNestCategory("Example");
+            BigDecimal num1 = new BigDecimal("100000000000");
+            BigDecimal num2 = new BigDecimal("99999999999");
+
+            cat.addExpense(num1);
+            cat.removeExpense(num2);
+
+            assertEquals(1, cat.CategorySpend());
+        }
+
+        /*
+         * Test with expense larger than spend
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void expenseLargerThanSpend() {
+
+            WhatNestCategory cat = new WhatNestCategory("Example");
+
+            BigDecimal budget = new BigDecimal("400");
+            cat.setCategoryBudget(budget);
+
+            BigDecimal spend = new BigDecimal("200");
+            cat.addExpense(spend);
+
+            assertThrows(Exception.class, () -> {
+                cat.removeExpense(new BigDecimal("400"));
+            });
+        }
+
+
+
+    }
+
+}
