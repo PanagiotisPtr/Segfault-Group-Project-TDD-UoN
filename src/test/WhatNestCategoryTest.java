95ad3653b3049a738f18d47fe3e29e20fc018254
unknown <psyll6@nottingham.ac.uk>
2019-03-28 13:40:00 +0000

    tests for 2.4.*

---
+++ src/test/WhatNestCategoryTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WhatNestCategoryTest {
+    @Nested
+    class testCategoryBudget {
+        /*
+         *Create Category object with name "Example" and budget to 12 and make
+         *sure that getCategoryBudget returns 12
+         * Written by: Lars Lunde 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void testCategoryBudget() {
+            WhatNestCategory cat = new WhatNestCategory("Example");
+            cat.setCategoryBudget(new BigDecimal("12.0"));
+            assertEquals(new BigDecimal("12.0"), cat.CategoryBudget());
+        }
+    }
+}
