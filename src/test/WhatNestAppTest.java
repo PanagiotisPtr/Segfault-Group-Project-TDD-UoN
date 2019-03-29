43f46c5059ebf660c46d276979f16f2926704856
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2019-03-29 01:57:22 +0000

    approved. Fixed typos. Replaced functors with lambda expression. BigDecimals should be initialized with strings (otherwise it's ambiguous). Removed -now- unusued import

--- src/test/WhatNestAppTest.java
+++ src/test/WhatNestAppTest.java
@@ -26,7 +26,7 @@ class WhatNestAppTest {
 
         /* 3.2.1 - Test with no transaction history
          * Written by: Matthew Smith 25/03/19
-         * Authorised by:
+         * Authorised by: Panagiotis Petridis
          */
         @Test
         void noTransactions() {
@@ -39,13 +39,13 @@ class WhatNestAppTest {
 
         /* 3.2.2 - Test with single transaction in history
          * Written by: Matthew Smith 25/03/19
-         * Authorised by:
+         * Authorised by: Panagiotis Petridis
          */
         @Test
         void singleTransaction() {
             WhatNestApp.UserTransactions = new ArrayList<>();
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans1",new BigDecimal(4.39), 0));
+                    new WhatNestTransaction("trans1",new BigDecimal("4.39"), 0));
             WhatNestApp.UserCategories = new ArrayList<>();
             WhatNestApp.UserCategories.add(
                     new WhatNestCategory("cat1"));
@@ -56,52 +56,52 @@ class WhatNestAppTest {
 
         /* 3.2.3 - Test with multiple valid items in transaction history.
          * Written by: Matthew Smith 25/03/19
-         * Authorised by:
+         * Authorised by: Panagiotis Petridis
          */
         @Test
         void manyTransactions() {
             WhatNestApp.UserTransactions = new ArrayList<>();
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans1",new BigDecimal(4.39), 0));
+                    new WhatNestTransaction("trans1",new BigDecimal("4.39"), 0));
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans2",new BigDecimal(43.21), 0));
+                    new WhatNestTransaction("trans2",new BigDecimal("43.21"), 0));
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans3",new BigDecimal(2.57), 1));
+                    new WhatNestTransaction("trans3",new BigDecimal("2.57"), 1));
             WhatNestApp.UserCategories = new ArrayList<>();
             WhatNestApp.UserCategories.add(new WhatNestCategory("cat1"));
             WhatNestApp.UserCategories.add(new WhatNestCategory("cat3"));
 
             WhatNestApp.ListTransactions();
-            assertEquals("1) trans1 (cat1) - £4.39\n2) trans1 (cat1) - £43.21\n2) trans1 (cat1) - £2.57\n"
+            assertEquals("1) trans1 (cat1) - £4.39\n2) trans2 (cat2) - £43.21\n2) trans3 (cat3) - £2.57\n"
                     , output.toString());
         }
 
         /* 3.2.4 - Test with unicode input
          * Written by: Matthew Smith 25/03/19
-         * Authorised by:
+         * Authorised by: Panagiotis Petridis
          */
         @Test
         void unicode() {
             WhatNestApp.UserTransactions = new ArrayList<>();
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("πρόγραμμα",new BigDecimal(4.39), 0));
+                    new WhatNestTransaction("Συναλλαγή",new BigDecimal("4.39"), 0));
             WhatNestApp.UserCategories = new ArrayList<>();
             WhatNestApp.UserCategories.add(
-                    new WhatNestCategory("λμασφα"));
+                    new WhatNestCategory("κατηγορία"));
 
             WhatNestApp.ListTransactions();
-            assertEquals("1) πρόγραμμα (λμασφα) - £4.39\n", output.toString());
+            assertEquals("1) Συναλλαγή (κατηγορία) - £4.39\n", output.toString());
         }
 
         /* 3.2.5 - Test with no numbers in decimal place
          * Written by: Matthew Smith 25/03/19
-         * Authorised by:
+         * Authorised by: Panagiotis Petridis
          */
         @Test
         void noDecimals() {
             WhatNestApp.UserTransactions = new ArrayList<>();
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans1",new BigDecimal(4), 0));
+                    new WhatNestTransaction("trans1",new BigDecimal("4"), 0));
             WhatNestApp.UserCategories = new ArrayList<>();
             WhatNestApp.UserCategories.add(
                     new WhatNestCategory("cat1"));
@@ -112,13 +112,13 @@ class WhatNestAppTest {
 
         /* 3.2.6 - Test with 1 number in decimal place
          * Written by: Matthew Smith 25/03/19
-         * Authorised by:
+         * Authorised by: Panagiotis Petridis
          */
         @Test
         void oneDecimal() {
             WhatNestApp.UserTransactions = new ArrayList<>();
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans1",new BigDecimal(4.1), 0));
+                    new WhatNestTransaction("trans1",new BigDecimal("4.1"), 0));
             WhatNestApp.UserCategories = new ArrayList<>();
             WhatNestApp.UserCategories.add(
                     new WhatNestCategory("cat1"));
@@ -129,58 +129,51 @@ class WhatNestAppTest {
 
         /* 3.2.7 - Test with out of range category
          * Written by: Matthew Smith 25/03/19
-         * Authorised by:
+         * Authorised by: Panagiotis Petridis
          */
         @Test
         void badCategory() {
             WhatNestApp.UserTransactions = new ArrayList<>();
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans1",new BigDecimal(4.1), 1));
+                    new WhatNestTransaction("trans1",new BigDecimal("4.1"), 1));
             WhatNestApp.UserCategories = new ArrayList<>();
             WhatNestApp.UserCategories.add(
                     new WhatNestCategory("cat1"));
 
-
-            assertThrows(
-                    ArrayIndexOutOfBoundsException.class,
-                    new Executable() {
-                        @Override
-                        public void execute() throws Throwable {
-                            WhatNestApp.ListTransactions();
-                        }
-                    }
-            );
+            assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+                WhatNestApp.ListTransactions();
+            });
         }
 
         /* 3.2.8 - Test with too many decimal places
          * Written by: Matthew Smith 25/03/19
-         * Authorised by:
+         * Authorised by: Panagiotis Petridis
          */
         @Test
         void manyDecimals() {
             WhatNestApp.UserTransactions = new ArrayList<>();
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans1",new BigDecimal(4.123), 0));
+                    new WhatNestTransaction("trans1",new BigDecimal("4.123"), 0));
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans1",new BigDecimal(4.127), 0));
+                    new WhatNestTransaction("trans2",new BigDecimal("4.127"), 0));
             WhatNestApp.UserCategories = new ArrayList<>();
             WhatNestApp.UserCategories.add(
                     new WhatNestCategory("cat1"));
 
             WhatNestApp.ListTransactions();
-            assertEquals("1) trans1 (cat1) - £4.12\n1) trans2 (cat1) - £4.13\n"
+            assertEquals("1) trans1 (cat1) - £4.12\n2) trans2 (cat1) - £4.13\n"
                     , output.toString());
         }
 
         /* 3.2.9 - Test with large value
          * Written by: Matthew Smith 25/03/19
-         * Authorised by:
+         * Authorised by: Panagiotis Petridis
          */
         @Test
         void largeValue() {
             WhatNestApp.UserTransactions = new ArrayList<>();
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans1",new BigDecimal(1000000), 0));
+                    new WhatNestTransaction("trans1",new BigDecimal("1000000"), 0));
             WhatNestApp.UserCategories = new ArrayList<>();
             WhatNestApp.UserCategories.add(
                     new WhatNestCategory("cat1"));
@@ -191,13 +184,13 @@ class WhatNestAppTest {
 
         /* 3.2.10 - Test with transaction of value 0
          * Written by: Matthew Smith 25/03/19
-         * Authorised by:
+         * Authorised by: Panagiotis Petridis
          */
         @Test
         void zeroValue() {
             WhatNestApp.UserTransactions = new ArrayList<>();
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans1",new BigDecimal(0), 0));
+                    new WhatNestTransaction("trans1",new BigDecimal("0"), 0));
             WhatNestApp.UserCategories = new ArrayList<>();
             WhatNestApp.UserCategories.add(
                     new WhatNestCategory("cat1"));
@@ -208,13 +201,13 @@ class WhatNestAppTest {
 
         /* 3.2.11 - Test with transaction of negative value
          * Written by: Matthew Smith 25/03/19
-         * Authorised by:
+         * Authorised by: Panagiotis Petridis
          */
         @Test
         void negativeValue() {
             WhatNestApp.UserTransactions = new ArrayList<>();
             WhatNestApp.UserTransactions.add(
-                    new WhatNestTransaction("trans1",new BigDecimal(-10.21), 0));
+                    new WhatNestTransaction("trans1",new BigDecimal("-10.21"), 0));
             WhatNestApp.UserCategories = new ArrayList<>();
             WhatNestApp.UserCategories.add(
                     new WhatNestCategory("cat1"));
