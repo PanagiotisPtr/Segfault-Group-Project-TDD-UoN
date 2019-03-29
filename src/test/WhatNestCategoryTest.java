fa81ed3e5dadf68e8892399ea9c1280718a40262
taradilley <pmytrd@nottingham.ac.uk>
2019-03-29 13:05:45 +0000

    Finished 2.9, 2.11, 2.12 tests

--- src/test/WhatNestCategoryTest.java
+++ src/test/WhatNestCategoryTest.java
@@ -7,17 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WhatNestCategoryTest {
     @Nested
-
     class removeExpense{
 
         /*
-         * Checks that BigDecimal type is accepted
+         * Checks that valid inputs give correct output
          * Written by: Tara Dilley 28/03/2019
          * Approved by:
          */
         @Test
-        void acceptsBigDecimal() {
-
+        void validValuesCheck() {
             WhatNestCategory cat = new WhatNestCategory("Example");
             BigDecimal num1 = new BigDecimal("100.00");
             BigDecimal num2 = new BigDecimal("1.3458934895");
@@ -50,7 +48,6 @@ class WhatNestCategoryTest {
          */
         @Test
         void noNegative() {
-
             WhatNestCategory cat = new WhatNestCategory("Example");
             assertThrows(Exception.class, () -> {
                 cat.removeExpense(new BigDecimal("-10000.12"));
@@ -64,7 +61,6 @@ class WhatNestCategoryTest {
          */
         @Test
         void largePositive() {
-
             WhatNestCategory cat = new WhatNestCategory("Example");
             BigDecimal num1 = new BigDecimal("100000000000");
             BigDecimal num2 = new BigDecimal("99999999999");
@@ -72,7 +68,7 @@ class WhatNestCategoryTest {
             cat.addExpense(num1);
             cat.removeExpense(num2);
 
-            assertEquals(1, cat.CategorySpend());
+            assertEquals(0, cat.CategorySpend().compareTo(num1.subtract(num2)));
         }
 
         /*
@@ -82,7 +78,6 @@ class WhatNestCategoryTest {
          */
         @Test
         void expenseLargerThanSpend() {
-
             WhatNestCategory cat = new WhatNestCategory("Example");
 
             BigDecimal budget = new BigDecimal("400");
@@ -95,9 +90,88 @@ class WhatNestCategoryTest {
                 cat.removeExpense(new BigDecimal("400"));
             });
         }
+    }
 
+    @Nested
+    class getRemainingBudget {
+
+        /*
+         * Test that correct remaining budget is found
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void getRemainingBudgetTest() {
+            WhatNestCategory cat = new WhatNestCategory("Example");
+
+            BigDecimal budget = new BigDecimal("400");
+            cat.setCategoryBudget(budget);
+
+            BigDecimal spend = new BigDecimal("200");
+            cat.addExpense(spend);
+
+            BigDecimal result = new BigDecimal("200");
+            assertEquals(0, cat.getRemainingBudget().compareTo(result));
+        }
+    }
+
+    @Nested
+    class toStringOverride{
+
+        /*
+         * Checks output when values are all 0
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void checkZero() {
+            WhatNestCategory cat = new WhatNestCategory("Example");
+
+            BigDecimal num = new BigDecimal("0");
+            cat.setCategoryBudget(num);
+            cat.addExpense(num);
+
+            assertEquals(0, cat.toString().compareTo("[Example] (£0.00) - Est. £0.00 (£0.00 remaining)"));
+        }
+
+        /*
+         * Checks a valid non-zero test case
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void checkValidCase() {
+            WhatNestCategory cat = new WhatNestCategory("Example");
+
+            BigDecimal budget = new BigDecimal("400");
+            cat.setCategoryBudget(budget);
+
+            BigDecimal spend = new BigDecimal("200");
+            cat.addExpense(spend);
+
+            assertEquals(0, cat.toString().compareTo("[Example] (£400.00) - Est. £200.00 (£200.00 remaining)"));
+        }
+
+        /*
+         * Checks that values are correct when user has overspent
+         * Written by: Tara Dilley 28/03/2019
+         * Approved by:
+         */
+        @Test
+        void checkOverspentCase() {
+            WhatNestCategory cat = new WhatNestCategory("Example");
+
+            BigDecimal budget = new BigDecimal("400");
+            cat.setCategoryBudget(budget);
+
+            BigDecimal spend = new BigDecimal("500");
+            cat.addExpense(spend);
+
+            assertEquals(0, cat.toString().compareTo("[Example] (£400.00) - Est. £500.00 (£100.00 overspent)"));
+        }
 
 
     }
+
 
 }
