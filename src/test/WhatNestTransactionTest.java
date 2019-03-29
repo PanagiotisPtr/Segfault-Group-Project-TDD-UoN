72cad70c7cabac55dc7e132eba1a2262241e53b7
Panagiotis Petridis <psypp3@nottingham.ac.uk>
2019-03-29 13:24:08 +0000

    approved

--- src/test/WhatNestTransactionTest.java
+++ src/test/WhatNestTransactionTest.java
@@ -9,65 +9,53 @@ import java.math.BigDecimal;
 class WhatNestTransactionTest {
     @Nested
     class TransactionCategory {
-        /*Tests for 1.5.*
-          Written by: Panagiotis Petridis
-          Date: 25/03/2019
-          Authorised by:
-        */
 
-        // Check that the value can handle zero - 1.5.1
-
-        // Test with constructor
+        /* Check that the value can handle zero - 1.5.1
+         * Written by: Panagiotis Petridis 29/03/19
+         * Authorised by: Lars Lunde
+         */
         @Test
         void testZeroCategoryConstructor() {
+            // Test with constructor
             WhatNestTransaction testTransaction =
                     new WhatNestTransaction("test", new BigDecimal("0"), 0);
             assertEquals(0, testTransaction.transactionCategory());
-        }
 
-        // Test with setTransactionCategory()
-        @Test
-        void testZeroCategorySetFunction() {
-            WhatNestTransaction testTransaction = new WhatNestTransaction();
+            // Test with setTransactionCategory()
             testTransaction.setTransactionCategory(0);
             assertEquals(0, testTransaction.transactionCategory());
         }
 
-        // Check that the value can handle negative numbers - 1.5.2
-
-        // Test with constructor
+        /* Check that the value can handle negative numbers - 1.5.2
+         * Written by: Panagiotis Petridis 29/03/19
+         * Authorised by: Lars Lunde
+         */
         @Test
         void testNegativeCategoryConstructor() {
+            // Test with constructor
             WhatNestTransaction testTransaction =
                     new WhatNestTransaction("test", new BigDecimal("0"), -10);
             assertEquals(-10, testTransaction.transactionCategory());
-        }
 
-        // Test with setTransactionCategory()
-        @Test
-        void testNegativeCategorySetFunction() {
-            WhatNestTransaction testTransaction = new WhatNestTransaction();
+            // Test with setTransactionCategory()
             testTransaction.setTransactionCategory(-10);
             assertEquals(-10, testTransaction.transactionCategory());
         }
 
-        // Check that the value can handle positive numbes - 1.5.3
-
-        // Test with constructor
+        /* Check that the value can handle positive numbes - 1.5.3
+         * Written by: Panagiotis Petridis 29/03/19
+         * Authorised by: Lars Lunde
+         */
         @Test
         void testPositiveCategoryConstructor() {
+            // Test with constructor
             WhatNestTransaction testTransaction =
                     new WhatNestTransaction("test", new BigDecimal("0"), 10);
             assertEquals(10, testTransaction.transactionCategory());
-        }
 
-        // Test with setTransactionCategory()
-        @Test
-        void testPositiveCategorySetFunction() {
-            WhatNestTransaction testTransaction = new WhatNestTransaction();
+            // Test with setTransactionCategory()
             testTransaction.setTransactionCategory(10);
             assertEquals(10, testTransaction.transactionCategory());
         }
-
     }
 }
